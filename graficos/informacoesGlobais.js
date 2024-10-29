a75c04100d4074502a757fa267dcb2d32b7e8f9c
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-10-29 20:49:00 -0300

    fix3

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,7 +8,7 @@ async function vizualizarInformacoesGlobais() {
     
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas <br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.` // template string
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo} bilhões</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio} horas conectadas</span>` // template string
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
