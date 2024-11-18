919c6e91843be4345ed2da269308b77bf999cc0f
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-11-18 22:45:16 +0000

    Final do site

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -7,18 +7,40 @@ async function quantidadeUsuarios() {
   const nomeDasRedes = Object.keys(dados)
   const quantidadeDeUsuarios = Object.values(dados)
 
+  
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  
+  const colors = nomeDasRedes.map(() => getRandomColor());
+
   const data = [
     {
       x: nomeDasRedes,
       y: quantidadeDeUsuarios,
       type: 'bar',
+      name: 'Quantidade de Usuários', 
       marker: {
-        color: getCSS('--color-bar')
+        color: colors, 
+        line: {
+          color: getComputedStyle(document.body).getPropertyValue('--bg-color'), 
+          width: 2                     
+        }
       }
     }
   ]
 
   const layout = {
+    margin: { b: 150 },  
+    xaxis: {
+        tickangle: -45  
+    },
     plot_bgcolor: getCSS('--bg-color'),
     paper_bgcolor: getCSS('--bg-color'),
     title: {
@@ -31,29 +53,32 @@ async function quantidadeUsuarios() {
       }
     },
     xaxis: {
+      tickfont: tickConfig,
       title: {
-        text: 'Instituições',
+        text: 'nome das redes sociais',
         font: {
           color: getCSS('--secondary-color')
-        }
+        }, 
+        standoff: 20 
       }
     },
     yaxis: {
+      tickfont: tickConfig,
       title: {
         text: 'bilhões de usuários ativos',
         font: {
           color: getCSS('--secondary-color')
-        }
+        },
+        standoff: 20 
       }
-    }
+    },
+    height: 600 
   }
 
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
   Plotly.newPlot(grafico, data, layout)
-
 }
 
 quantidadeUsuarios()
-
